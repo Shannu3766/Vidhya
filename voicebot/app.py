b3a81ecae0e4bf90ac5c341a2747f1f66f1e4760
Amareswar <116486017+kasaamareswar@users.noreply.github.com>
2024-11-13 11:22:55 +0530

    New Package

--- voicebot/app.py
+++ voicebot/app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO
 import os
-from groq import Groq
+# from groq import Groq
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a strong secret key
 
 # Initialize SocketIO with the app
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 
 # Initialize Groq client
-client = Groq(api_key='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')
+# client = Groq(api_key='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')
 
 # Function to generate a request based on user input
 def generate_request(user_input):
@@ -32,7 +31,7 @@ def generate_request(user_input):
 # Route for the home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('main.html')
 
 # API route to handle user input and generate a response
 @app.route('/get_response', methods=['POST'])
